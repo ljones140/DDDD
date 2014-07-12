d96b0cf344a6587012cd18245490eae87bf782b6
Lewis Jones <ljones140@gmail.com>
2014-07-12 14:58:01 +0100

    Setting up the DB connections to Flyingra_dddd

--- connections.php
+++ connections.php
@@ -1,1 +1,7 @@
-php
+<?php
+	// Define databaseconnection constants
+	define ('DB_HOST' , '10.168.1.47');
+	define ('DB_USER', 'flyingra_dddd');
+	define ('DB_PASSWORD', 'monkeydbuser');
+	define ('DB_NAME', 'flyingra_dddd');
+?>
