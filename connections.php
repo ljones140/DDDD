d304f0a1f343c7300bcbba7b145411c961417e67
Lewis Jones <ljones140@gmail.com>
2014-07-14 22:02:31 +0100

    Removed db connections

--- connections.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Define databaseconnection constants
-	define ('DB_HOST' , '10.168.1.47');
-	define ('DB_USER', 'flyingra_dddd');
-	define ('DB_PASSWORD', 'monkeydbuser');
-	define ('DB_NAME', 'flyingra_dddd');
-?>
